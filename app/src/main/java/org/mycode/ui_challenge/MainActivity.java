b401f28a1202c9a5cce40c9ea9b5ad69fb2e060b
vvduth <ducthai060501@gmail.com>
2021-07-22 23:59:07 +0300

    "Fix Layout "

--- app/src/main/java/org/mycode/ui_challenge/MainActivity.java
+++ app/src/main/java/org/mycode/ui_challenge/MainActivity.java
@@ -3,11 +3,13 @@ package org.mycode.ui_challenge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button thisisbutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
